66ef37338f60afd8b671c7a788d9f52db0b5f879
CRWayman <39571113+CRWayman@users.noreply.github.com>
2022-05-24 09:06:52 -0400

    Minor changes to init function and yaml write out (#57)
    * Minor changes to init function and yaml write out
    
    * Fixing style issues
    
    * Fixing code style and adding method to process check button selections (#81)
    
    Co-authored-by: Dan Holdaway <27729500+danholdaway@users.noreply.github.com>

--- src/swell/suites/swell_gui.py
+++ src/swell/suites/swell_gui.py
@@ -6,8 +6,6 @@ import yaml
 with open('hofx/suite_page_hofx.yaml', 'r') as yml:
     widget_dict = yaml.safe_load(yml)
 
-LARGEFONT = 14
-
 
 class tkinterApp(tk.Tk):
 
@@ -19,7 +17,10 @@ class tkinterApp(tk.Tk):
         container = tk.Frame(self)
         container.pack(side="top", fill="both", expand=True)
 
+        # Set aesthetics
+
         self.geometry("600x500")
+        self.LARGEFONT = 18
 
         # initializing frames to an empty array
         self.frames = {}
@@ -41,17 +42,18 @@ class tkinterApp(tk.Tk):
         frame = self.frames[cont]
         frame.tkraise()
 
-# first window frame startpage
 
-
+# Landing Page
 class StartPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
 
         # Button to go to HofX Model
-        label = ttk.Label(self, text="Startpage", font=LARGEFONT).pack()
+        label = ttk.Label(self, text="Startpage",
+                          font=controller.LARGEFONT).pack()
         button1 = ttk.Button(self, text="HofX",
-                             command=lambda: controller.show_frame(Model)).pack()
+                             command=lambda:
+                             controller.show_frame(Model)).pack()
 
 
 # Main Application for Model Component
@@ -63,17 +65,29 @@ class Model(tk.Frame):
             self.check_widget_type(widget)
 
         # Submit Button
-        submit = ttk.Button(self, text='Generate YAML', command=self.send_to_file)
+        submit = ttk.Button(self, text='Generate YAML',
+                            command=self.send_to_file)
         submit.pack(side=tk.LEFT, padx=5, pady=5)
         # Back Button
-        backbutton = ttk.Button(self, text="Back", command=lambda: controller.show_frame(StartPage))
+        backbutton = ttk.Button(self, text="Back",
+                                command=lambda:
+                                controller.show_frame(StartPage))
         backbutton.pack(side=tk.LEFT, padx=5, pady=5)
 
     def send_to_file(self):
+        self.yaml_dict = {}
+        self.cb_dict = {}
         for widget in self.widget_inputs:
             field = widget[0]['name']
-            value = widget[1].get()
-            print('%s: "%s"' % (field, value))
+            if widget[0]['widget type'] == 'check button':
+                self.check_checks(widget)
+                value = self.cb_dict
+            else:
+                value = widget[1].get()
+                self.yaml_dict[field] = value
+            self.yaml_dict[field] = value
+        with open('gui_experiment.yaml', 'w') as outfile:
+            yaml.dump(self.yaml_dict, outfile, default_flow_style=False)
 
     def check_widget_type(self, widget):
         self.widget = widget
@@ -90,7 +104,7 @@ class Model(tk.Frame):
 
     def make_entry(self):
         row = tk.Frame(self)
-        lab = tk.Label(row, width=15, text=self.widget['name'], anchor='w')
+        lab = tk.Label(row, width=20, text=self.widget['name'], anchor='w')
         ent = tk.Entry(row)
         row.pack(side=tk.TOP, fill=tk.X, padx=5, pady=5)
         lab.pack(side=tk.LEFT)
@@ -98,7 +112,8 @@ class Model(tk.Frame):
         self.widget_inputs.append((self.widget, ent))
 
     def make_radio_btn(self):
-        tk.Label(self, text=self.widget['name'], justify=tk.LEFT, padx=5).pack()
+        tk.Label(self, text=self.widget['name'],
+                 justify=tk.LEFT, padx=5).pack()
         v = tk.IntVar()
         v.set(1)
         options = self.widget['options']
@@ -112,8 +127,8 @@ class Model(tk.Frame):
         self.widget_inputs.append((self.widget, v))
 
     def make_dropdown(self):
-        tk.Label(self, text=self.widget['name'], justify=tk.LEFT, padx=5).pack()
-
+        tk.Label(self, text=self.widget['name'],
+                 justify=tk.LEFT, padx=5).pack()
         clicked = tk.StringVar()
         clicked.set(self.widget['options'][0])
         tk.OptionMenu(self, clicked, *self.widget['options']).pack()
@@ -136,6 +151,10 @@ class Model(tk.Frame):
             vlist.append(v)
 
         self.widget_inputs.append((self.widget, vlist))
+
+    def check_checks(self, check_button):
+        for i, opt in enumerate(check_button[0]['options']):
+            self.cb_dict[opt] = check_button[1][i].get()
 
 
 # Driver Code
