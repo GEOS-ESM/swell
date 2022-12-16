88428ec7cbdcccdb62267d2b04153d083c9e84d4
dooruk <dorukhan.ardag@nasa.gov>
2022-12-16 17:30:07 -0500

    bump execution with seg fault

--- src/swell/tasks/generate_b_climatology.py
+++ src/swell/tasks/generate_b_climatology.py
@@ -7,18 +7,56 @@
 # -----------------------------------------------
 import os
 import shutil
+import subprocess
+
+import yaml
 
 from swell.tasks.base.task_base import taskBase
 
 # --------------------------------------------------------------------------------------------------
 
+
+interface_executable = {
+  'soca': 'soca_staticbinit.x',
+}
+
+# --------------------------------------------------------------------------------------------------
+
 class GenerateBClimatology(taskBase):
+
+    def jedi_dictionary_iterator(self, jedi_config_dict):
+
+        # Loop over dictionary and replace if value is a dictionary
+        # ---------------------------------------------------------
+        for key, value in jedi_config_dict.items():
+            if isinstance(value, dict):
+                self.jedi_dictionary_iterator(value)
+            else:
+                if 'TASKFILL' in value:
+                    value_file = value.replace('TASKFILL', '')
+                    value_dict = self.open_jedi_interface_model_config_file(value_file)
+                    jedi_config_dict[key] = value_dict
+
+    # ----------------------------------------------------------------------------------------------
+
+    def generate_jedi_config(self):
+
+        # Create dictionary from the templated JEDI config file
+        # ---------------------------------------------------------
+        jedi_config_dict = self.open_jedi_oops_config_file('bump')
+
+        # Read configs for the rest of the dictionary
+        # -------------------------------------------
+        self.jedi_dictionary_iterator(jedi_config_dict)
+
+        return jedi_config_dict
+
+    # ----------------------------------------------------------------------------------------------
 
     def execute(self):
         """Acquires B Matrix files 
 
             TODO: Generate Bump files for any np if not created already
-            TODO: get config instead of 'soca' in the b_dir
 
            Parameters
            ----------
@@ -32,6 +70,12 @@ class GenerateBClimatology(taskBase):
         vertical_resolution = self.config_get('vertical_resolution')
         background_error_model = self.config_get('background_error_model')
         cycle_dir = self.config_get('cycle_dir')
+        experiment_dir = self.config_get('experiment_dir')
+
+        # Get the JEDI interface for this model component
+        # -----------------------------------------------
+        model_component_meta = self.open_jedi_interface_meta_config_file()
+        jedi_interface = model_component_meta['jedi_interface']
 
         if background_error_model == 'bump':
             
@@ -50,13 +94,58 @@ class GenerateBClimatology(taskBase):
 
             # Load experiment file
             # --------------------
-            b_dir = os.path.join(swell_static_files, 'jedi', 'soca', model_component, 
-                        background_error_model,'climatological',resolution,str(np))
+            b_dir = os.path.join(swell_static_files, 'jedi', jedi_interface, 
+                        model_component, background_error_model, 'climatological', 
+                            resolution,str(np))
             
             d_dir = os.path.join(cycle_dir,'background_error_model')
 
             self.logger.info('  Copying BUMP files from: '+b_dir)
-            shutil.copytree(b_dir, d_dir, dirs_exist_ok=True)       
+            # shutil.copytree(b_dir, d_dir, dirs_exist_ok=True)
+
+            # Jedi configuration file
+            # -----------------------
+            jedi_config_file = os.path.join(cycle_dir, 'jedi_bump_config.yaml')
+            # Generate the JEDI configuration file for running the executable
+            # ---------------------------------------------------------------
+            jedi_config_dict = self.generate_jedi_config()
+            
+            with open(jedi_config_file, 'w') as jedi_config_file_open:
+                yaml.dump(jedi_config_dict, jedi_config_file_open, default_flow_style=False)
+
+            # Jedi executable name
+            # --------------------
+            jedi_executable = interface_executable[jedi_interface]
+            jedi_executable_path = os.path.join(experiment_dir, 'jedi_bundle', 
+                        'build', 'bin', jedi_executable)
+
+            # Run the JEDI executable
+            # -----------------------
+            self.logger.info('Running '+jedi_executable_path+' with '+str(np)+' processors.')
+
+            command = ['mpirun', '-np', str(np), jedi_executable_path, jedi_config_file]
+            
+            # Move to the cycle directory
+            # ---------------------------
+            os.chdir(cycle_dir)
+            os.mkdir('backgrond_error_model')
+            # Execute
+            # -------
+            process = subprocess.Popen(command, stdout=subprocess.PIPE)
+            while True:
+                output = process.stdout.readline().decode()
+                if output == '' and process.poll() is not None:
+                    break
+                if output:
+                    print(output.strip())
+            rc = process.poll()
+
+            # Abort task if the executable did not run successfully
+            # -----------------------------------------------------
+            if rc != 0:
+                command_string = ' '.join(command)
+                self.logger.abort('subprocess.run with command ' + command_string +
+                                ' failed to execute.', False)
 
         else:
             
