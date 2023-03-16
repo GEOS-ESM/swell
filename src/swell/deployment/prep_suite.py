cd1998e65b69a0efe8a3a1350234393ea9b130db
Dan Holdaway <27729500+danholdaway@users.noreply.github.com>
2023-03-16 11:49:02 -0400

    Only use Cascade Lake nodes for the jobs so they can be synced to the nogin node fabric (#159)
    Co-authored-by: danholdaway <danholdaway@users.noreply.github.com>

--- src/swell/deployment/prep_suite.py
+++ src/swell/deployment/prep_suite.py
@@ -59,7 +59,7 @@ def prepare_cylc_suite_jinja2(logger, swell_suite_path, exp_suite_path, experime
     render_dictionary['scheduling']['BuildJedi']['qos'] = 'allnccs'
     render_dictionary['scheduling']['BuildJedi']['nodes'] = 1
     render_dictionary['scheduling']['BuildJedi']['ntasks_per_node'] = 24
-    render_dictionary['scheduling']['BuildJedi']['constraint'] = 'cas|sky'
+    render_dictionary['scheduling']['BuildJedi']['constraint'] = 'cas'
 
     render_dictionary['scheduling']['BuildGeos'] = {}
     render_dictionary['scheduling']['BuildGeos']['execution_time_limit'] = 'PT1H'
@@ -67,7 +67,7 @@ def prepare_cylc_suite_jinja2(logger, swell_suite_path, exp_suite_path, experime
     render_dictionary['scheduling']['BuildGeos']['qos'] = 'allnccs'
     render_dictionary['scheduling']['BuildGeos']['nodes'] = 1
     render_dictionary['scheduling']['BuildGeos']['ntasks_per_node'] = 24
-    render_dictionary['scheduling']['BuildGeos']['constraint'] = 'cas|sky'
+    render_dictionary['scheduling']['BuildGeos']['constraint'] = 'cas'
 
     render_dictionary['scheduling']['GenerateBClimatology'] = {}
     render_dictionary['scheduling']['GenerateBClimatology']['execution_time_limit'] = 'PT15M'
@@ -75,7 +75,7 @@ def prepare_cylc_suite_jinja2(logger, swell_suite_path, exp_suite_path, experime
     render_dictionary['scheduling']['GenerateBClimatology']['qos'] = 'allnccs'
     render_dictionary['scheduling']['GenerateBClimatology']['nodes'] = 1
     render_dictionary['scheduling']['GenerateBClimatology']['ntasks_per_node'] = 24
-    render_dictionary['scheduling']['GenerateBClimatology']['constraint'] = 'cas|sky'
+    render_dictionary['scheduling']['GenerateBClimatology']['constraint'] = 'cas'
 
     render_dictionary['scheduling']['RunJediHofxExecutable'] = {}
     render_dictionary['scheduling']['RunJediHofxExecutable']['execution_time_limit'] = 'PT2H'
@@ -83,7 +83,7 @@ def prepare_cylc_suite_jinja2(logger, swell_suite_path, exp_suite_path, experime
     render_dictionary['scheduling']['RunJediHofxExecutable']['qos'] = 'allnccs'
     render_dictionary['scheduling']['RunJediHofxExecutable']['nodes'] = 1
     render_dictionary['scheduling']['RunJediHofxExecutable']['ntasks_per_node'] = 24
-    render_dictionary['scheduling']['RunJediHofxExecutable']['constraint'] = 'cas|sky'
+    render_dictionary['scheduling']['RunJediHofxExecutable']['constraint'] = 'cas'
 
     render_dictionary['scheduling']['RunJediVariationalExecutable'] = {}
     render_dictionary['scheduling']['RunJediVariationalExecutable']['execution_time_limit'] = 'PT1H'
@@ -91,7 +91,7 @@ def prepare_cylc_suite_jinja2(logger, swell_suite_path, exp_suite_path, experime
     render_dictionary['scheduling']['RunJediVariationalExecutable']['qos'] = 'allnccs'
     render_dictionary['scheduling']['RunJediVariationalExecutable']['nodes'] = 1
     render_dictionary['scheduling']['RunJediVariationalExecutable']['ntasks_per_node'] = 24
-    render_dictionary['scheduling']['RunJediVariationalExecutable']['constraint'] = 'cas|sky'
+    render_dictionary['scheduling']['RunJediVariationalExecutable']['constraint'] = 'cas'
 
     render_dictionary['scheduling']['RunGeosExecutable'] = {}
     render_dictionary['scheduling']['RunGeosExecutable']['execution_time_limit'] = 'PT2H'
@@ -99,7 +99,7 @@ def prepare_cylc_suite_jinja2(logger, swell_suite_path, exp_suite_path, experime
     render_dictionary['scheduling']['RunGeosExecutable']['qos'] = 'allnccs'
     render_dictionary['scheduling']['RunGeosExecutable']['nodes'] = 1
     render_dictionary['scheduling']['RunGeosExecutable']['ntasks_per_node'] = 24
-    render_dictionary['scheduling']['RunGeosExecutable']['constraint'] = 'cas|sky'
+    render_dictionary['scheduling']['RunGeosExecutable']['constraint'] = 'cas'
 
     # Render the template
     # -------------------
