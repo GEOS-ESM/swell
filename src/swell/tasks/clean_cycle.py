e846b81fa014a23f9bd315f35991e4612bb52932
danholdaway <danholdaway@users.noreply.github.com>
2023-04-13 16:52:51 -0400

    move to R2D2 for getting the observations

--- src/swell/tasks/clean_cycle.py
+++ src/swell/tasks/clean_cycle.py
@@ -27,6 +27,7 @@ class CleanCycle(taskBase):
 
     def execute(self):
 
+        # Parse config
         cycle_dir = self.config_get("cycle_dir")
         clean_list = self.config_get('clean_patterns', None)
 
@@ -34,20 +35,57 @@ class CleanCycle(taskBase):
         if clean_list is None:
             return
 
-        if os.path.isdir(cycle_dir):
-            os.chdir(cycle_dir)
-            # Remove all specified files
-            for pattern in clean_list:
-                if pattern == '*':
-                    continue
-                path_parts = os.path.split(pattern)
-                if path_parts[1] == '*':
-                    continue
-                else:
-                    for file_to_delete in glob.glob(path_parts[1]):
-                        os.remove(file_to_delete)
+        # Move to the cycle directory
+        os.chdir(cycle_dir)
 
-            # Save cycle_done file to cycle_dir
-            filename = 'cycle_done'
-            cmd = 'touch ' + os.path.join(cycle_dir, filename)
-            os.system(cmd)
+        # Remove all specified files
+        for pattern in clean_list:
+
+            # ---------------------------
+            # Perform some safety checks:
+            # ---------------------------
+
+            # 1. Check that path is not absolute. Things can only be deleted relative to the cycle
+            #    directory.
+            if os.path.isabs(pattern):
+                self.logger.abort(f'Absolute paths are forbidden. Offending entry: {pattern}')
+
+            # 2. Check that the pattern does not begin with a / or ./
+            if pattern[0] == '/' or pattern[0] == '.':
+                self.logger.abort(f'Patterns beginning with \'/\' or \'.\' are forbidden. '
+                                  f'Offending entry: {pattern}')
+
+            # 3. Check that the pattern is not a blanket removal of all files
+            if any(ele == '*' for ele in os.path.split(pattern)):
+                self.logger.abort(f'Deleting all files from any directory is forbidden. Offending '
+                                  f'entry: {pattern}')
+
+            # ---------------------------
+            # ---------------------------
+
+            # Assemble list of files to remove
+            items_to_remove = glob.glob(pattern)
+
+            # Loop over files and remove
+            for item_to_remove in items_to_remove:
+
+                # Print info about what will be removed
+                self.logger.info(f'Removing item {item_to_remove}')
+
+                # Only allow removing of empty directories
+                if os.path.isdir(item_to_remove):
+
+                    if len(os.listdir(item_to_remove)) == 0:
+                        os.rmdir(item_to_remove)
+                    else:
+                        self.logger.info(f'Trying to remove directory {item_to_remove} but code '
+                                         f'can only remove empty directories. Reorder removal '
+                                         f'to empty directory first.')
+
+                else:
+
+                    os.remove(item_to_remove)
+
+        # Save cycle_done file to cycle_dir
+        with open(os.path.join(cycle_dir, 'cycle_done'), 'w') as fp:
+            pass
