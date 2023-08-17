98f478e58aac5afe2e328c66b4ff3bd4179fbbdd
dooruk <dorukhan.ardag@nasa.gov>
2023-08-17 10:44:13 -0400

    bump generation works with the new task questions

--- src/swell/tasks/generate_b_climatology.py
+++ src/swell/tasks/generate_b_climatology.py
@@ -16,9 +16,9 @@ from swell.utilities.shell_commands import run_track_log_subprocess
 # --------------------------------------------------------------------------------------------------
 
 
-interface_executable = {
-  'soca': 'soca_staticbinit.x',
-}
+# interface_executable = {
+#   'soca': 'soca_error_covariance_toolbox.x',
+# }
 
 # --------------------------------------------------------------------------------------------------
 
@@ -35,7 +35,7 @@ class GenerateBClimatology(taskBase):
             else:
                 if 'TASKFILL' in value:
                     value_file = value.replace('TASKFILL', '')
-                    value_dict = self.open_jedi_interface_model_config_file(value_file)
+                    value_dict = self.jedi_rendering.render_interface_model(value_file)
                     jedi_config_dict[key] = value_dict
 
     # ----------------------------------------------------------------------------------------------
@@ -64,61 +64,50 @@ class GenerateBClimatology(taskBase):
 
     def generate_bump(self):
 
-        # Folder name contains both horizontal and vertical resolutions
-        # ----------------------------
-        resolution = self.horizontal_resolution + 'x' + self.vertical_resolution
+        self.logger.info(' Generating BUMP files.')
 
-        # Get the name of the model component
-        # --------------------------------
-        model_component = self.get_model()
+        # Jedi configuration file
+        # -----------------------
+        jedi_config_file = os.path.join(self.cycle_dir(), 'jedi_bump_config.yaml')
 
-        # Load experiment file
+        # Generate the JEDI configuration file for running the executable
+        # ---------------------------------------------------------------
+        jedi_config_dict = self.generate_jedi_config()
+
+        with open(jedi_config_file, 'w') as jedi_config_file_open:
+            yaml.dump(jedi_config_dict, jedi_config_file_open, default_flow_style=False)
+
+        # Jedi executable name
         # --------------------
-        b_dir = os.path.join(self.swell_static_files, 'jedi', self.jedi_interface,
-                             model_component, self.background_error_model, 'climatological',
-                             resolution, str(self.np))
+        # jedi_executable = interface_executable[self.jedi_interface]
+        # jedi_executable_path = os.path.join(self.experiment_path(), 'jedi_bundle',
+        #                                     'build', 'bin', jedi_executable)
 
-        d_dir = os.path.join(self.cycle_dir(), 'background_error_model')
+        # Get the JEDI interface metadata
+        # -------------------------------
+        model_component_meta = self.jedi_rendering.render_interface_meta()
 
-        try:
-            self.logger.info('  Copying BUMP files from: '+b_dir)
-            shutil.copytree(b_dir, d_dir, dirs_exist_ok=True)
+        # Jedi executable name
+        # --------------------
+        jedi_executable = model_component_meta['executables']['bump']
+        jedi_executable_path = os.path.join(self.experiment_path(), 'jedi_bundle',
+                                            'build', 'bin', jedi_executable)
 
-        except Exception:
-            self.logger.info('  Copying failed, generating BUMP files.')
+        # Run the JEDI executable
+        # -----------------------
+        self.logger.info('Running '+jedi_executable_path+' with '+str(self.np)+' processors.')
 
-            # Jedi configuration file
-            # -----------------------
-            jedi_config_file = os.path.join(self.cycle_dir(), 'jedi_bump_config.yaml')
+        command = ['mpirun', '-np', str(self.np), jedi_executable_path, jedi_config_file]
 
-            # Generate the JEDI configuration file for running the executable
-            # ---------------------------------------------------------------
-            jedi_config_dict = self.generate_jedi_config()
+        # Move to the cycle directory
+        # ---------------------------
+        os.chdir(self.cycle_dir())
+        if not os.path.exists('background_error_model'):
+            os.mkdir('background_error_model')
 
-            with open(jedi_config_file, 'w') as jedi_config_file_open:
-                yaml.dump(jedi_config_dict, jedi_config_file_open, default_flow_style=False)
-
-            # Jedi executable name
-            # --------------------
-            jedi_executable = interface_executable[self.jedi_interface]
-            jedi_executable_path = os.path.join(self.experiment_path(), 'jedi_bundle',
-                                                'build', 'bin', jedi_executable)
-
-            # Run the JEDI executable
-            # -----------------------
-            self.logger.info('Running '+jedi_executable_path+' with '+str(self.np)+' processors.')
-
-            command = ['mpirun', '-np', str(self.np), jedi_executable_path, jedi_config_file]
-
-            # Move to the cycle directory
-            # ---------------------------
-            os.chdir(self.cycle_dir())
-            if not os.path.exists('background_error_model'):
-                os.mkdir('background_error_model')
-
-            # Execute
-            # -------
-            run_track_log_subprocess(self.logger, command)
+        # Execute
+        # -------
+        run_track_log_subprocess(self.logger, command)
 
         return
 
@@ -139,6 +128,10 @@ class GenerateBClimatology(taskBase):
             See the taskBase constructor for more information.
         """
 
+        # Parse configuration
+        # -------------------
+        window_offset = self.config.window_offset()
+        window_type = self.config.window_type()
         self.swell_static_files = self.config.swell_static_files()
         self.horizontal_resolution = self.config.horizontal_resolution()
         self.vertical_resolution = self.config.vertical_resolution()
@@ -148,16 +141,30 @@ class GenerateBClimatology(taskBase):
         self.jedi_rendering.add_key('npx_proc', self.config.npx_proc(None))
         self.jedi_rendering.add_key('npy_proc', self.config.npy_proc(None))
         self.jedi_rendering.add_key('total_processors', self.config.total_processors(None))
+        self.jedi_rendering.add_key('analysis_variables', self.config.analysis_variables())
+
+        # Compute data assimilation window parameters
+        # -------------------------------------------
+        local_background_time = self.da_window_params.local_background_time(window_offset,
+                                                                            window_type)
+        local_background_time_iso = self.da_window_params.local_background_time_iso(window_offset,
+                                                                                    window_type)
+
+        # Background
+        # ----------
+        self.jedi_rendering.add_key('local_background_time_iso', local_background_time_iso)
+        self.jedi_rendering.add_key('local_background_time', local_background_time)
+
         model_component_meta = self.jedi_rendering.render_interface_meta()
         self.jedi_interface = model_component_meta['jedi_interface']
 
         # Compute number of processors
         # ----------------------------
-        self.np = eval(model_component_meta['total_processors'])
+        self.np = eval(str(model_component_meta['total_processors']))
 
-        # Obtain and initialize proper error model
+        # Obtain and initialize proper error model (only bump for now)
         # -----------------------------------------------
-        self.background_error_model = self.config.background_error_model()
+        self.background_error_model = 'bump'
         self.initialize_background()
 
 # --------------------------------------------------------------------------------------------------
