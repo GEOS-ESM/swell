e7a03a491a6ea2eaae49f7c69b1462253d1149cf
asewnath <akira.sewnath@gmail.com>
2023-11-08 10:04:42 -0500

    fix observing sys test

--- src/swell/test/code_tests/generate_observing_system_test.py
+++ src/swell/test/code_tests/generate_observing_system_test.py
@@ -3,8 +3,10 @@ from swell.utilities.observing_system_records import ObservingSystemRecords
 
 observations = ['amsua_n19']
 observing_system_records_path = './yaml_output/'
-path_to_geosana_gridcomp = '/discover/nobackup/asewnath/github/GEOSana_GridComp/'
-path_to_gsi_records = os.path.join(path_to_geosana_gridcomp, 'GEOSaana_GridComp',
+
+# Clone GeosAna
+os.system('git clone https://github.com/GEOS-ESM/GEOSana_GridComp.git')
+path_to_gsi_records = os.path.join('GEOSana_GridComp/', 'GEOSaana_GridComp',
                                    'GSI_GridComp', 'mksi', 'sidb')
 sat_records = ObservingSystemRecords()
 sat_records.parse_records(path_to_gsi_records)
