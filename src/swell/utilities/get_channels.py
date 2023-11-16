1cf00b814841adf9a914e4394d70c4d2bec18265
asewnath <akira.sewnath@gmail.com>
2023-11-16 16:23:46 -0500

    stash

--- src/swell/utilities/get_channels.py
+++ src/swell/utilities/get_channels.py
@@ -10,6 +10,7 @@
 import yaml
 import os
 from datetime import datetime as dt
+from itertools import groupby
 
 # --------------------------------------------------------------------------------------------------
 
@@ -32,6 +33,22 @@ def process_channel_lists(channel_list):
             final_channels_list += [int(element)]
 
     return final_channels_list
+
+# --------------------------------------------------------------------------------------------------
+
+
+def create_range_string(avail_list):
+    '''
+        Function converts integer list into string of ranges
+    '''
+    ranges = []
+    for _, g in groupby(enumerate(avail_list), lambda i_x:i_x[0]-i_x[1]):
+        group = list(map(lambda x: x[1], g))
+        if len(group) > 1:
+            ranges.append(f'{group[0]}-{group[-1]}')
+        else:
+            ranges.append(str(group[0]))
+    return ', '.join(ranges)
 
 # --------------------------------------------------------------------------------------------------
 
@@ -70,10 +87,11 @@ def get_channels(path_to_observing_sys_yamls, observation, dt_cycle_time):
             active_channels = get_channel_list(data['active'], dt_cycle_time)
 
         available_channels_list = process_channel_lists(available_channels)
+        available_range_string = create_range_string(available_channels_list)
         active_channels_list = process_channel_lists(active_channels)
         use_flags = [1 if x in active_channels_list else -1 for x in available_channels_list]
 
-        return available_channels_list, use_flags
+        return available_range_string, use_flags
 
     else:
         return None
