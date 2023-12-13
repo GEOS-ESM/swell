c1ef0fde118840443baeb05c4cc2a533d4adf68b
asewnath <akira.sewnath@gmail.com>
2023-12-13 09:33:16 -0500

    addressing comment

--- src/swell/utilities/get_channels.py
+++ src/swell/utilities/get_channels.py
@@ -94,6 +94,6 @@ def get_channels(path_to_observing_sys_yamls, observation, dt_cycle_time):
         return available_range_string, use_flags
 
     else:
-        return None
+        return None, None
 
 # --------------------------------------------------------------------------------------------------
