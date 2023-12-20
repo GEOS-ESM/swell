8e45b6d5240c439cdd28d870873c165a3a26d339
CEgerer93 <cegerer@protonmail.com>
2023-12-20 11:53:55 -0500

    Add new task questions in prep for computing hofx for ensemble members

---
+++ src/swell/tasks/run_jedi_hofx_ensemble_executable.py
@@ -0,0 +1,29 @@
+# (C) Copyright 2021- United States Government as represented by the Administrator of the
+# National Aeronautics and Space Administration. All Rights Reserved.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+
+
+# --------------------------------------------------------------------------------------------------
+
+
+import os
+import yaml
+
+from swell.tasks.base.task_base import taskBase
+from swell.utilities.run_jedi_executables import jedi_dictionary_iterator, run_executable
+
+
+# --------------------------------------------------------------------------------------------------
+
+
+class RunJediHofxEnsembleExecutable(taskBase):
+
+    # ----------------------------------------------------------------------------------------------
+
+    def execute(self):
+        pass
+
+
+# --------------------------------------------------------------------------------------------------
