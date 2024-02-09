387f1a73544ed6def917cce04a5ed406bcb825bb
Doruk Ardağ <38666458+Dooruk@users.noreply.github.com>
2024-02-09 10:18:44 -0500

    Update modules and keep static files in shared locations (#296)
    * modules updated to the project location
    
    * use dimensions instead of variables
    
    * where clause updates
    
    * temporary jedi builds
    
    * erase the temporary script
    
    * temporarily erase jedi builds
    
    * shared project location
    
    * use lower resolution for ocean hofx tests
    
    * take sst_ostia out
    
    * codestyle fix

--- src/swell/test/code_tests/missing_obs_test.py
+++ src/swell/test/code_tests/missing_obs_test.py
@@ -31,7 +31,8 @@ jedi_rendering = JediConfigRendering(logger, experiment_root,
 jedi_rendering.add_key('window_begin', '20211211T210000Z')
 jedi_rendering.add_key('background_time', '20211211T150000Z')
 jedi_rendering.add_key('crtm_coeff_dir',
-                       '/discover/nobackup/drholdaw/SwellStaticFiles/jedi/crtm_coefficients/2.4.1/')
+                       '/discover/nobackup/projects/gmao/advda/SwellStaticFiles/' +
+                       'jedi/crtm_coefficients/2.4.1/')
 jedi_rendering.set_obs_records_path(path_to_observing_sys_yamls)
 
 # Test 1: Observation file does not exist
