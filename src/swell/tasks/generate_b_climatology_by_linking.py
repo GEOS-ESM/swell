fda2f2d407db46d1afe1a829bb050b38814448c2
Doruk Ardağ <38666458+Dooruk@users.noreply.github.com>
2024-03-06 11:58:55 -0500

    Use `EXPLICIT_DIFFUSION` as Static Background for `geos_marine` (#301)
    * abandon bump, welcome diffusion
    
    * diffusion works with static files, filepaths needed to be shorter
    
    * calc_scales relevant modifications
    
    * parameters_diffusion_vt related changes
    
    * fix passive observations
    
    * use SLES12 as default for now
    
    * allow generate climatology to run
    
    * code test fixes
    
    * minor eva and obs changes
    
    * update GEOSgcm version to v11.5.1
    
    * pyyaml version update
    
    * update f90nml version
    
    python 3.12 dependencies
    
    * netcdf version bump

--- src/swell/tasks/generate_b_climatology_by_linking.py
+++ src/swell/tasks/generate_b_climatology_by_linking.py
@@ -5,8 +5,6 @@
 # which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
 
 # --------------------------------------------------------------------------------------------------
-
-
 import os
 
 from swell.tasks.base.task_base import taskBase
@@ -21,11 +19,19 @@ class GenerateBClimatologyByLinking(taskBase):
     def execute(self):
         """Acquires B Matrix files for background error model(s):
 
-            - Bump:
-            Tries fetching existing bump files (contingent upon the number of
-            total processors), creates new ones in 'cycle_dir' otherwise.
+            - EXPLICIT_DIFFUSION:
 
-            - TODO GSI:
+            For geos_marine, horizontal corr. scales solely depend on the tripolar grid
+            hence they are located in the swell_static_files directory. Meanwhile,
+            vertical corr. scales are MLD dependent. Hence, this task will check
+            for the vertical diffusion files corresponding to the current cycle.
+            If it fails, it will move onto generate these files via the
+            GenerateBClimatology task.
+
+            - BUMP:
+
+            This one is currently not supported. However, it will likely become
+            an option again once higher resolution grid tests are executed.
 
         Parameters
         ----------
@@ -34,6 +40,7 @@ class GenerateBClimatologyByLinking(taskBase):
         """
 
         # Get the flavor of static background error model
+        # -----------------------------------------------
         background_error_model = self.config.background_error_model()
 
         # Get the JEDI interface for this model component
@@ -41,63 +48,65 @@ class GenerateBClimatologyByLinking(taskBase):
         self.jedi_rendering.add_key('background_error_model', self.config.background_error_model())
 
         # Extract general parts of the config
+        # -----------------------------------
         swell_static_files_main = self.config.swell_static_files()
         swell_static_files_user = self.config.swell_static_files_user(None)
 
         # Set the destination directory
+        # ------------------------------
         target_path = os.path.join(self.cycle_dir(), 'background_error_model')
         os.makedirs(target_path, mode=0o777, exist_ok=True)
 
         # Source path base the part that looks like /path/to/static_background_error/
+        # -------------------------------------------------------------------------
         source_path_base = os.path.join('jedi', 'interfaces', self.get_model(), 'model',
                                         'static_background_error', background_error_model)
 
+        # Compute data assimilation window parameters to obtain the local background time
+        # -------------------------------------------------------------------------------
+        window_offset = self.config.window_offset()
+        window_type = self.config.window_type()
+        local_background_time = self.da_window_params.local_background_time(window_offset,
+                                                                            window_type)
+
+        # Background
+        # ----------
+        self.jedi_rendering.add_key('local_background_time', local_background_time)
+
         # Model specific part of the path, i.e. anything that goes between the base path above,
         # and the actual files that need to be linked into the directory.
-        if background_error_model == 'bump':
-            source_path_model = self.append_source_path_bump()
+        # The name of the correlation file is also defined, which could be useful in the
+        # long run.
+        # ------------------------------------------------------------------------------
+        if background_error_model == 'explicit_diffusion':
+            horizontal_resolution = self.config.horizontal_resolution()
+            vertical_resolution = self.config.vertical_resolution()
+            res_path = horizontal_resolution + 'x' + vertical_resolution
+
+            correlation_files = 'vt.' + local_background_time + '.nc'
         else:
-            self.logger.abort("Only bump is currently supported")
+            self.logger.abort("Only explicit diffusion is supported")
 
         # List of source paths to search for some existing files. A limitation of this code is that
         # it does not know the name of the files that should be linked. It will link all files that
         # it finds in the first directory that some are found in. The first place to look is a user
         # provided path containing swell static files. The user would be responsible for filling
-        # those directories in a way that is compliant with the above. Secondly is searches the
+        # those directories in a way that is compliant with the above. Secondly, it searches the
         # centrally controlled swell static files directory.
 
         source_paths = []
         # First, append with any user provided path
+        # ------------------------------------------
         if swell_static_files_user is not None:
             source_paths.append(os.path.join(swell_static_files_user, source_path_base,
-                                             source_path_model))
+                                             res_path, correlation_files))
         # Second, append with centrally controlled path
+        # ----------------------------------------------
         source_paths.append(os.path.join(swell_static_files_main, source_path_base,
-                                         source_path_model))
+                                         res_path, correlation_files))
 
         # First check the users swell static files path
+        # ----------------------------------------------
         link_all_files_from_first_in_hierarchy_of_sources(self.logger, source_paths, target_path)
-
-    # ----------------------------------------------------------------------------------------------
-
-    def append_source_path_bump(self):
-
-        # First part of bump path is the model resolution
-        horizontal_resolution = self.config.horizontal_resolution()
-        vertical_resolution = self.config.vertical_resolution()
-        res_path = horizontal_resolution + 'x' + vertical_resolution
-
-        # Second part of bump path is the number of processors
-        npx_proc = self.config.npx_proc(None)
-        npy_proc = self.config.npy_proc(None)
-
-        if npx_proc is None:
-            npx_proc = self.config.total_processors()
-            npy_proc = 1
-
-        proc_path = str(npx_proc) + 'x' + str(npy_proc)
-
-        return os.path.join(res_path, proc_path)
-
 
 # --------------------------------------------------------------------------------------------------
