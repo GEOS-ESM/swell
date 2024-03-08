eeeeaf919d0ac837c1de348535a7997d9e138104
asewnath <akira.sewnath@gmail.com>
2024-03-08 16:33:01 -0500

    check if default check works

--- src/swell/test/code_tests/question_dictionary_comparison_test.py
+++ src/swell/test/code_tests/question_dictionary_comparison_test.py
@@ -22,8 +22,8 @@ class QuestionDictionaryTest(unittest.TestCase):
     def test_dictionary_comparison(self):
 
         # Run main task question dictionary generation
-        tq_dicts_rc = tq_dicts()
-        assert tq_dicts_rc == 0
+        # tq_dicts_rc = tq_dicts()
+        # assert tq_dicts_rc == 0
 
         # Run generation for defaults
         tq_dicts_defaults_rc = tq_dicts_defaults()
