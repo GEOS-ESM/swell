c7d33afbcf18d8ca5ab612e0ab56c06d82a9650e
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2024-08-08 16:41:42 -0400

    Support swell tier1 tests from swell CLI

--- src/swell/test/suite_tests/suite_tests.py
+++ src/swell/test/suite_tests/suite_tests.py
@@ -1,6 +1,5 @@
 import tempfile
 import yaml
-# import shutil
 
 from pathlib import Path
 from datetime import datetime
@@ -9,42 +8,61 @@ from swell.deployment.create_experiment import create_experiment_directory
 from swell.deployment.launch_experiment import launch_experiment
 
 def run_suite(suite: str):
-    print(f"Suite: {suite}")
     experiment_id = f"t{datetime.now().strftime('%Y%jT%H%M')}{suite}"
 
-    # with tempfile.TemporaryDirectory() as rundir:
-    rundir = tempfile.TemporaryDirectory()
+    # Get test directory from `~/.swell/swell-test.yml`
+    yamlfile = Path("~/.swell/swell-test.yaml").expanduser()
+    try:
+        with open(yamlfile, "r") as f:
+            test_config = yaml.safe_load(f)
+    except FileNotFoundError:
+        print(f"User test config ({yamlfile}) found. Using defaults.")
+        test_config = {"test_root": Path(tempfile.TemporaryDirectory().name)}
+    except Exception as err:
+        raise(err)
 
-    testdir = Path(rundir.name)
+    testdir = Path(test_config["test_root"]).expanduser()
+    testdir.mkdir(exist_ok=True, parents=True)
 
-    print(f"Storing test results in {testdir}")
+    print(f"Testing suite: {suite}")
+    print(f"Test directory: {testdir}")
+    print(f"Experiment ID: {experiment_id}")
 
     override = {
         "experiment_id": experiment_id,
         "experiment_root": str(testdir)
     }
-    override_yml = testdir / "override.yaml"
+
+    experiment_dir = testdir / experiment_id
+    experiment_dir.mkdir(parents=True, exist_ok=True)
+
+    override_yml = experiment_dir / "override.yaml"
     with open(override_yml, "w") as f:
         yaml.dump(override, f)
-
-    # Setup
-    # cylc_run_path = Path.home() / "cylc-run" / f"{experiment_id}-suite"
-    # try:
-    #     shutil.rmtree(cylc_run_path)
-    # except FileNotFoundError:
-    #     pass
 
     create_experiment_directory(suite, "defaults", "nccs_discover_sles15", str(override_yml), False, None)
 
     # TODO: Check some stuff about the experiment directory
 
-    suite_path = str(testdir / experiment_id / f"{experiment_id}-suite")
-    log_path = str(testdir / "log")
+    suite_path = str(experiment_dir / f"{experiment_id}-suite")
+    log_path = str(experiment_dir / "log")
 
     launch_experiment(suite_path, True, log_path)
 
     # TODO: Check the outputs
 
-def suite_tests():
-    for suite in ("3dvar", "hofx", "ufo_testing"):
+if __name__ == "__main__":
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser(description = "Swell suite tests")
+    parser.add_argument("suites", nargs="+",
+                        help="Suite(s) to run (or `all` to run all suites)")
+
+    args = parser.parse_args()
+    if args.suites == ["all"]:
+        suites = ("3dvar", "hofx", "ufo_testing")
+    else:
+        suites = args.suites
+
+    for suite in suites:
         run_suite(suite)
