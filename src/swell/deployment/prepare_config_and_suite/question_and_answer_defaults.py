256595d5fd466253acc1325d56de92492ce97066
Michael Anstett <michael.anstett@nasa.gov>
2024-09-13 14:49:42 -0400

    more types in hint

--- src/swell/deployment/prepare_config_and_suite/question_and_answer_defaults.py
+++ src/swell/deployment/prepare_config_and_suite/question_and_answer_defaults.py
@@ -8,9 +8,12 @@
 # --------------------------------------------------------------------------------------------------
 
 
+from typing import Union
+from datetime import datetime as dt
+
 class GetAnswerDefaults:
 
-    def get_answer(self, key: str, val: dict) -> str:
+    def get_answer(self, key: str, val: dict) -> Union[int,float,str,dt]:
         return val['default_value']
 
 # --------------------------------------------------------------------------------------------------
