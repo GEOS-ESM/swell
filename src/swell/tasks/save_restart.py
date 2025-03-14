1deb6bdcdf88675439ffb7d03067b0a197a4b95d
dooruk <dorukhan.ardag@nasa.gov>
2025-03-14 14:19:25 -0400

    save restart and increments to R2D2 folder

--- src/swell/tasks/save_restart.py
+++ src/swell/tasks/save_restart.py
@@ -7,13 +7,15 @@
 
 # --------------------------------------------------------------------------------------------------
 
-from datetime import datetime
+from datetime import datetime as dt
 import isodate
 import os
+from r2d2 import store
 
 from swell.tasks.base.task_base import taskBase
 from swell.utilities.datetime_util import datetime_formats
-from swell.utilities.file_system_operations import move_files, copy_to_dst_dir
+from swell.utilities.file_system_operations import copy_to_dst_dir
+from swell.utilities.r2d2 import create_r2d2_config
 
 # --------------------------------------------------------------------------------------------------
 
@@ -23,75 +25,80 @@ class SaveRestart(taskBase):
     def execute(self):
 
         """
-        Moving history files to R2D2DataStore
+        Moving background and increment files to R2D2DataStore
+        This is a temporary solution until we have a proper v3 data storage
+        Does not handle 4d backgrounds properly
         """
 
-        save_folder = self.experiment_id()
-        current_cycle = os.path.basename(os.path.dirname(self.forecast_dir()))
-        cc_dto = self.cycle_time_dto()
-        forecast_duration = 'P1DT12H'
-        forecast_offset_dur = isodate.parse_duration(forecast_duration)
+        # Parse config
+        window_type = self.config.window_type()
+        window_length = self.config.window_length()
+        window_offset = self.config.window_offset()
+        forecast_duration = self.config.forecast_duration()
+        self.jedi_rendering.add_key('marine_models', self.config.marine_models(None))
+        r2d2_local_path = self.config.r2d2_local_path()
 
-        beg_dto = cc_dto - isodate.parse_duration('PT12H')
-        # --------------------------------------------------------------
-        dst_dto = cc_dto - forecast_offset_dur
+        # Position relative to center of the window where forecast starts
+        background_time_offset = self.config.background_time_offset()
+        forecast_start_time = self.cycle_time_dto() - isodate.parse_duration(background_time_offset)
 
-        # print(forecast_offset_dur)
-        # print(forecast_duration)
-        print(beg_dto)
-        print(dst_dto)
-        src = self.forecast_dir('his_' + cc_dto.strftime('%Y_%m_%d_%H') + '.nc')
+        # Convert to datetime durations
+        local_background_time = self.da_window_params.local_background_time(window_offset,
+                                                                            window_type)
+        local_background_time_iso = self.da_window_params.local_background_time_iso(window_offset,
+                                                                                    window_type)
+        analysis_time_iso = self.da_window_params.analysis_time_iso(window_type, self.suite_name())
+        self.jedi_rendering.add_key('local_background_time', local_background_time)
+        self.jedi_rendering.add_key('local_background_time_iso', local_background_time_iso)
+        self.jedi_rendering.add_key('analysis_time_iso', analysis_time_iso)
 
-        mainf = f'/discover/nobackup/dardag/R2D2DataStore/Local/mom6_cice6_UFS/fc/{save_folder}/global/1440x1080'
+        # Set R2D2 config file
+        # --------------------
+        create_r2d2_config(self.logger, self.platform(), self.cycle_dir(), r2d2_local_path)
 
-        if not os.path.exists(mainf):
-            os.makedirs(mainf, exist_ok = True)
-        # Convert fc_fto to 2021-06-01 format
-        # -----------------------------------
-        folder = dst_dto.strftime('%Y-%m-%d')
+        # Get r2d2 dictionary
+        r2d2_dict = self.jedi_rendering.render_interface_model('r2d2')
 
-        dst_date = datetime.strftime(dst_dto, datetime_formats['iso_format'])
-        dst = f'mom6_cice6_UFS.{save_folder}.fc.global.MOM.res.' + dst_date + '.P1DT12H.nc'
+        # Loop over fc
+        for fc in r2d2_dict['store']['fc']:
+            store(date=forecast_start_time,
+                  source_file=fc['filename'],
+                  model='mom6_cice6_UFS',
+                  file_type=fc['file_type'],
+                  fc_date_rendering='analysis',
+                  step=window_length,
+                  resolution=self.config.horizontal_resolution(),
+                  type='fc',
+                  experiment=self.experiment_id())
 
-        dst = os.path.join(mainf, folder, dst)
+        # Loop over an
+        for an in r2d2_dict['store']['an']:
+            store(date=self.cycle_time_dto(),
+                  source_file=an['filename'],
+                  model='mom6_cice6_UFS',
+                  file_type=an['file_type'],
+                  fc_date_rendering='analysis',
+                  resolution=self.config.horizontal_resolution(),
+                  type='an',
+                  experiment=self.experiment_id())
 
-        # Create RESTART folder
-        # ---------------------
-        if not os.path.exists(os.path.join(mainf, folder)):
-            os.mkdir(os.path.join(mainf, folder))
+        # Oceanstats needs special handling from the forecast folder. It is produced at the end of
+        # the forecast and could be saved as a good metric. We are replicating the same structure as
+        # the R2D2 files.
+        mainf = os.path.join(r2d2_local_path, 'mom6_cice6_UFS',
+                             'fc',
+                             self.experiment_id(),
+                             'global',
+                             self.config.horizontal_resolution())
 
-        self.logger.info(f"Moving history to: {dst}")
-        move_files(self.logger, src, dst)
+        dst_date = dt.strftime(forecast_start_time, datetime_formats['iso_format'])
 
-        # Define other copy commands
-        # --------------------------
-        # Copy ice file
-        src_date_fgat = datetime.strftime(beg_dto, datetime_formats['directory_format'])
-        src = os.path.join(self.cycle_dir(), 'cice.res.' + src_date_fgat + '.nc')
-        self.logger.info(f"Moving ice to: {dst}")
-        # dst = f'ice.{save_folder}.fc.global.MOM.res.' + dst_date + '.P1DT12H.nc'
-        dst = f'mom6_cice6_UFS.{save_folder}.fc.global.cice.res.' + dst_date + '.P1DT12H.nc'
-        dst = os.path.join(mainf, folder, dst)
-        move_files(self.logger, src, dst)
+        # Oceanstats is produced at the end of the forecast
+        src_stats = os.path.join(self.forecast_dir(), 'ocean.stats.nc')
+        dst_stats = os.path.join(mainf, forecast_start_time.strftime('%Y-%m-%d'),
+                                 f'mom6_cice6_UFS.{self.experiment_id()}.fc.global.MOM.oceanstats.'
+                                 + dst_date + '.' + forecast_duration + '.nc')
 
-        # Copy increment file(s)
-        src_inc = os.path.join(self.cycle_dir(), 'mom6_increment.nc')
-        dst_inc = os.path.join(mainf, folder, f'ocn.{save_folder}.fc.global.MOM.inc.' + dst_date + '.P1DT12H.nc')
-        copy_to_dst_dir(self.logger, src_inc, dst_inc)
-
-        #ice.cice6_02.incr.2021-07-02T18:00:00Z.nc
-        src_inc = os.path.join(self.cycle_dir(), f'ice.{save_folder}.incr.{cc_dto.strftime("%Y-%m-%dT%H:%M:%SZ")}.nc')
-        dst_inc = os.path.join(mainf, folder, f'ice.{save_folder}.fc.global.MOM.inc.' + dst_date + '.P1DT12H.nc')
-        copy_to_dst_dir(self.logger, src_inc, dst_inc)
-
-        # # Copy bkgerrgodas.nc file
-        #src_bkgerr = os.path.join(self.cycle_dir(), 'bkgerrgodas.nc')
-        #dst_bkgerr = os.path.join(mainf, folder, f'mom6_cice6_UFS.{save_folder}.fc.global.MOM.bkgerr.' + dst_date + '.P1DT12H.nc')
-        #copy_to_dst_dir(self.logger, src_bkgerr, dst_bkgerr)
-
-        # # Copy oceanstats file
-        src_bkgerr = os.path.join(self.forecast_dir(), 'ocean.stats.nc')
-        dst_bkgerr = os.path.join(mainf, folder, f'mom6_cice6_UFS.{save_folder}.fc.global.MOM.oceanstats.' + dst_date + '.P1DT12H.nc')
-        copy_to_dst_dir(self.logger, src_bkgerr, dst_bkgerr)
+        copy_to_dst_dir(self.logger, src_stats, dst_stats)
 
 # --------------------------------------------------------------------------------------------------
