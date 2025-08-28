12404472c403366befe7c73984f2feb18b777587
ftgoktas <ftgoktas@gmail.com>
2025-08-28 16:45:23 -0500

    Add scripts for manual setup for R2D2 #318

---
+++ scripts/load_r2d2.sh
@@ -0,0 +1,5 @@
+# load spack-stack modules
+mod_swell
+# load r2d2-client installation
+module use -a /discover/nobackup/projects/gmao/advda/JediOpt/modulefiles/core
+module load r2d2-client/sles15_0604
