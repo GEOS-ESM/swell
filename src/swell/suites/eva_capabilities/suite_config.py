dbcee3be9bf55483a4479e31d938a2a0cf494920
gmao-msienkie <gmao-msienkie@users.noreply.github.com>
2025-10-15 18:05:16 -0400

    Add tcvitals observations

--- src/swell/suites/eva_capabilities/suite_config.py
+++ src/swell/suites/eva_capabilities/suite_config.py
@@ -96,7 +96,8 @@ class SuiteConfig(QuestionContainer, Enum):
                 "sfcship",
                 "sfc",
                 "sondes",
-                "ssmis_f17"
+                "ssmis_f17",
+                "tcp"
             ]),
             qd.ncdiag_experiments(['x0050_fgat']),
             qd.clean_patterns(['*.txt', '*.csv']),
