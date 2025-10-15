35c85d6acf611bcc2fd7878d37fe10bcdaea64fd
gmao-msienkie <gmao-msienkie@users.noreply.github.com>
2025-10-15 18:29:36 -0400

    change 'background' to 'observation'

--- src/swell/tasks/get_obs_not_in_r2d2.py
+++ src/swell/tasks/get_obs_not_in_r2d2.py
@@ -26,7 +26,7 @@ class GetObsNotInR2d2(taskBase):
         # --------------------
         cycle_date = self.__datetime__.string_directory()
 
-        # Get the path and pattern for the background files
+        # Get the path and pattern for the observation files
         # -------------------------------------------------
         existing_path = self.config.ioda_locations_not_in_r2d2()
 
@@ -50,8 +50,8 @@ class GetObsNotInR2d2(taskBase):
 
         # Assert that some files were found
         # ---------------------------------
-        self.logger.assert_abort(len(existing_path_files) > 0, f'No background '
-                                 'files matching cycle in background directory.')
+        self.logger.assert_abort(len(existing_path_files) > 0, f'No observation '
+                                 'files matching cycle in observation directory.')
 
         # Loop over all the files
         # -----------------------
