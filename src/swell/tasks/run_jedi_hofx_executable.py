67ce963cb36950a3c3ebfeec2ed41ab83e7bbdf1
Michael Anstett <michael.anstett@nasa.gov>
2026-01-07 09:47:38 -0500

    Update src/swell/tasks/run_jedi_hofx_executable.py
    Co-authored-by: Maryam Abdi-Oskouei <mary.abdi@gmail.com>

--- src/swell/tasks/run_jedi_hofx_executable.py
+++ src/swell/tasks/run_jedi_hofx_executable.py
@@ -37,7 +37,6 @@ class RunJediHofxExecutable(taskBase):
         window_length = self.config.window_length()
         background_time_offset = self.config.background_time_offset()
         observations = self.config.observations()
-        background_time_offset = self.config.background_time_offset()
         jedi_forecast_model = self.config.jedi_forecast_model(None)
         generate_yaml_and_exit = self.config.generate_yaml_and_exit(False)
         save_geovals = self.config.save_geovals(False)
