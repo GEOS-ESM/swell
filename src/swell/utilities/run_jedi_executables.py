30717df82a16e5874b7dc07d4cef724ff08ee450
Maryam Abdi-Oskouei <mary.abdi@gmail.com>
2026-01-08 17:37:21 -0500

    write missing obs in jedi.yaml

--- src/swell/utilities/run_jedi_executables.py
+++ src/swell/utilities/run_jedi_executables.py
@@ -104,13 +104,7 @@ def jedi_dictionary_iterator(
                     obs_list = obs.copy()
                     for ob in obs_list:
                         obs_dict = jedi_rendering.render_interface_observations(ob)
-                        use_observation = check_obs(jedi_rendering.observing_system_records_path,
-                                                    ob, obs_dict, cycle_time)
-                        if use_observation:
-                            observations.append(obs_dict)
-                        else:
-                            # Remove observation from obs list passed into function
-                            obs.remove(ob)
+                        observations.append(obs_dict)
                     jedi_config_dict[key] = observations
 
                 elif value_special == 'model' and window_type == '4D':
