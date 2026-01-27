bf42dd30f3dc9ae66cddd0941ab482eb622a8401
Maryam Abdi-Oskouei <mary.abdi@gmail.com>
2026-01-26 19:07:50 -0500

    first commit

---
+++ src/swell/configuration/jedi/interfaces/geos_cf/model/shared.py
@@ -0,0 +1,19 @@
+# (C) Copyright 2021- United States Government as represented by the Administrator of the
+# National Aeronautics and Space Administration. All Rights Reserved.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+
+# --------------------------------------------------------------------------------------------------
+
+field_io_names = {
+    'eastward_wind': 'ua',
+    'northward_wind': 'va',
+    'air_temperature': 'T',
+    'air_pressure_at_surface': 'PS',
+    'air_pressure_thickness': 'DELP',
+    'volume_mixing_ratio_of_no2': 'NO2',
+    'volume_mixing_ratio_of_no': 'NO', # need to fix this.
+    'volume_mixing_ratio_of_o3': 'O3',
+    'volume_mixing_ratio_of_co': 'CO'
+}
---
+++ src/swell/suites/hofx_cf/suite_config.py
@@ -0,0 +1,35 @@
+# --------------------------------------------------------------------------------------------------
+#  @package configuration
+#
+#  Class containing the configuration. This is a dictionary that is converted from
+#  an input yaml configuration file. Various function are included for interacting with the
+#  dictionary.
+#
+# --------------------------------------------------------------------------------------------------
+
+
+from swell.utilities.swell_questions import QuestionContainer, QuestionList
+from swell.utilities.question_defaults import QuestionDefaults as qd
+from swell.suites.suite_questions import SuiteQuestions as sq
+
+from enum import Enum
+
+
+# --------------------------------------------------------------------------------------------------
+
+class SuiteConfig(QuestionContainer, Enum):
+
+    # --------------------------------------------------------------------------------------------------
+
+    hofx_cf = QuestionList(
+        list_name="hofx_cf",
+        questions=[
+            sq.common,
+            qd.start_cycle_point("2023-08-05T18:00:00Z"),
+            qd.final_cycle_point("2023-08-05T18:00:00Z"),
+            qd.jedi_build_method("use_existing"),
+            qd.model_components(['geos_cf']),
+        ],
+        geos_cf=[
+        ]
+    )
