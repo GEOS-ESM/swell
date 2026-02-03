e40af4fafdf14fd666da858a0078a99f3e1f0b5b
Maryam Abdi-Oskouei <mary.abdi@gmail.com>
2026-02-02 19:42:09 -0500

    fix coding norm

--- src/swell/suites/hofx_cf/suite_config.py
+++ src/swell/suites/hofx_cf/suite_config.py
@@ -29,8 +29,9 @@ class SuiteConfig(QuestionContainer, Enum):
             qd.final_cycle_point("2023-08-05T18:00:00Z"),
             qd.jedi_build_method("use_existing"),
             qd.model_components(['geos_cf']),
-            qd.check_for_obs(False) # don't check empty for empty obs
+            qd.check_for_obs(False)  # don't check empty for empty obs
         ],
+
         geos_cf=[
         ]
     )
