555e0bf394a8f1be5c84ac1eafcc0038d1627b76
Yonggang Yu <yonggang.yu@nasa.gov>
2026-02-03 16:52:27 -0500

    Adapt yamls to saber HybridBlockChain (saber PR: 1129)

--- src/swell/configuration/jedi/interfaces/geos_marine/model/background_error_diffusion_vt.py
+++ src/swell/configuration/jedi/interfaces/geos_marine/model/background_error_diffusion_vt.py
@@ -14,6 +14,7 @@ from collections.abc import Mapping
 def background_error_diffusion_vt(template_dict: Mapping) -> Mapping:
     background_error_diffusion_vt = {
         'covariance model': 'SABER',
+        'covariance type': 'gsi hybrid covariance',
         'saber central block': {
             'saber block name': 'diffusion',
             'calibration': {
