ea37090d29a98630d743a89f26f77ee1c1404a2e
jeromebarre <jerome.e.barre@gmail.com>
2026-03-09 16:21:30 -0400

    update stage b matrix files

--- src/swell/configuration/jedi/interfaces/geos_cf/model/stage_cycle.py
+++ src/swell/configuration/jedi/interfaces/geos_cf/model/stage_cycle.py
@@ -15,14 +15,26 @@ def stage_cycle(template_dict: Mapping) -> Mapping:
 
     cycle_dir = template_dict['cycle_dir']
     swell_static_files = template_dict['swell_static_files']
+    npx_proc = template_dict['npx_proc']
+    npy_proc = template_dict['npy_proc']
 
     stage_cycle = [
         {'copy_files': {
             'directories': [
-                [f'{swell_static_files}/jedi/interfaces/geos_cf/namelists/*', f'{cycle_dir}/']
+                [f'{swell_static_files}/jedi/interfaces/geos_cf/fv3_files/*', f'{cycle_dir}/']
             ]
         }}
     ]
+
+    # Only link NICAS files if saber_central_block is bump_nicas
+    if template_dict.get('saber_central_block') == 'bump_nicas':
+        stage_cycle.append({
+            'link_files': {
+                'directories': [
+                    [f'{swell_static_files}/jedi/interfaces/geos_cf/nicas/layout_{npx_proc}x{npy_proc}x6/*', f'{cycle_dir}/nicas/']
+                ]
+            }
+        })
 
     return stage_cycle
 
