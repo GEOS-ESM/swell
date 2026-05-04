51e75f0076f75351a49a64d1f0017f8bf3a33f1c
Maryam Abdi-Oskouei <mary.abdi@gmail.com>
2026-05-04 12:14:01 -0400

    save test

--- src/swell/tasks/run_forecast.py
+++ src/swell/tasks/run_forecast.py
@@ -14,16 +14,18 @@ from swell.utilities.shell_commands import run_subprocess
 
 # --------------------------------------------------------------------------------------------------
 
-
 class RunForecast(taskBase):
 
-    def execute(self) -> None:
-        """Submits the GCM forecast job script via sbatch."""
 
-        cycle_dir = self.cycle_dir()
-        gcm_run_script = os.path.join(cycle_dir, 'scratch', 'gcm_run_geoscf.j')
+   def execute(self) -> None:
+       """Runs the GCM forecast job script directly."""
 
-        self.logger.info(f'Submitting forecast job and waiting for completion: {gcm_run_script}')
-        run_subprocess(self.logger, ['sbatch', '--wait', gcm_run_script])
+
+       cycle_dir = self.cycle_dir()
+       gcm_run_script = os.path.join(cycle_dir, 'scratch', 'gcm_run_geoscf.j')
+
+
+       self.logger.info(f'Running forecast job: {gcm_run_script}')
+       run_subprocess(self.logger, ['/bin/csh', gcm_run_script])
 
 # --------------------------------------------------------------------------------------------------
