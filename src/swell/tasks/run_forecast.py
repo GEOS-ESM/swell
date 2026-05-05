937384b1f3aa785f01adea5c0a4b0dc79342b5bc
Maryam Abdi-Oskouei <mary.abdi@gmail.com>
2026-05-05 14:56:53 -0400

    capture gcm_run.j log

--- src/swell/tasks/run_forecast.py
+++ src/swell/tasks/run_forecast.py
@@ -9,6 +9,7 @@
 
 import os
 
+from swell.swell_path import get_swell_path
 from swell.tasks.base.task_base import taskBase
 from swell.utilities.shell_commands import run_subprocess
 
@@ -18,12 +19,19 @@ from swell.utilities.shell_commands import run_subprocess
 class RunForecast(taskBase):
 
     def execute(self) -> None:
-        """Submits the GCM forecast job script via sbatch."""
+        """Submits gcm_run_geoscf.j via geoscf-run.sh, capturing the SLURM job log."""
 
         cycle_dir = self.cycle_dir()
-        gcm_run_script = os.path.join(cycle_dir, 'scratch', 'gcm_run_geoscf.j')
+        scratch_dir = os.path.join(cycle_dir, 'scratch')
 
-        self.logger.info(f'Submitting forecast job and waiting for completion: {gcm_run_script}')
-        run_subprocess(self.logger, ['sbatch', '--wait', gcm_run_script])
+        geoscf_run_sh = os.path.join(get_swell_path(), 'tasks', 'geoscf-run.sh')
+
+        self.logger.info(f'Running forecast via: {geoscf_run_sh}')
+        self.logger.info(f'Scratch directory: {scratch_dir}')
+
+        env = os.environ.copy()
+        env['SCRATCH_DIR'] = scratch_dir
+
+        run_subprocess(self.logger, ['/bin/bash', geoscf_run_sh], env=env)
 
 # --------------------------------------------------------------------------------------------------
