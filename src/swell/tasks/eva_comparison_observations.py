f680bfe2646278739cfe386f3370f6353919ac3c
Michael Anstett <michael.anstett@nasa.gov>
2026-05-29 12:08:41 -0400

    comparison obs not in parallel

--- src/swell/tasks/eva_comparison_observations.py
+++ src/swell/tasks/eva_comparison_observations.py
@@ -120,152 +120,144 @@ class EvaComparisonObservations(taskBase):
                              445, 552, 573, 906, 1121, 1194, 1427, 1585],
             }
 
-        # Loop over observations and create dictionaries
-        # ----------------------------------------------
-        eva_dicts = []  # Empty list of dictionaries
+        observation = self.get_parameter()
 
         # Set the observing system records path
         self.jedi_rendering.set_obs_records_path(self.config.observing_system_records_path(None))
 
-        for observation in observations:
-            if self.get_model() == 'geos_atmosphere':
-                obs_long_name = ioda_name_to_long_name(observation, self.logger)
+        if self.get_model() == 'geos_atmosphere':
+            obs_long_name = ioda_name_to_long_name(observation, self.logger)
+        else:
+            obs_long_name = observation
+
+        observation_dict_1 = None
+        for value in obs_config_1:
+            if value['obs space']['name'] == obs_long_name:
+                observation_dict_1 = value.copy()
+
+        observation_dict_2 = None
+        for value in obs_config_2:
+            if value['obs space']['name'] == obs_long_name:
+                observation_dict_2 = value.copy()
+
+        if observation_dict_1 is None or observation_dict_2 is None:
+            return
+
+        # Check if IODA observation input and output have non-zero location dimensions
+        use_obs_1 = check_obs(self.jedi_rendering.observing_system_records_path, observation,
+                                observation_dict_1, self.cycle_time_dto(), input_and_output=True)
+
+        use_obs_2 = check_obs(self.jedi_rendering.observing_system_records_path, observation,
+                                observation_dict_2, self.cycle_time_dto(), input_and_output=True)
+
+        use_obs = use_obs_1 and use_obs_2
+
+        if not use_obs:
+            return
+
+        # Split the full path into path and filename
+        obs_path_file_1 = observation_dict_1['obs space']['obsdataout']['engine']['obsfile']
+        cycle_dir_1, obs_file_1 = os.path.split(obs_path_file_1)
+
+        # Split the full path into path and filename
+        obs_path_file_2 = observation_dict_2['obs space']['obsdataout']['engine']['obsfile']
+        cycle_dir_2, obs_file_2 = os.path.split(obs_path_file_2)
+
+        # Check for need to add 0000 to the file
+        # --------------------------------------
+        if not os.path.exists(obs_path_file_1):
+            obs_path_file_name, obs_path_file_ext = os.path.splitext(obs_path_file_1)
+            obs_path_file_0000 = obs_path_file_name + '_0000' + obs_path_file_ext
+            if not os.path.exists(obs_path_file_0000):
+                self.logger.abort(f'No observation file found for {obs_path_file_1} or ' +
+                                    f'{obs_path_file_0000}')
+            obs_path_file_1 = obs_path_file_0000
+
+        # Check for need to add 0000 to the file
+        # --------------------------------------
+        if not os.path.exists(obs_path_file_2):
+            obs_path_file_name, obs_path_file_ext = os.path.splitext(obs_path_file_2)
+            obs_path_file_0000 = obs_path_file_name + '_0000' + obs_path_file_ext
+            if not os.path.exists(obs_path_file_0000):
+                self.logger.abort(f'No observation file found for {obs_path_file_2} or ' +
+                                    f'{obs_path_file_0000}')
+            obs_path_file_2 = obs_path_file_0000
+
+        # Get instrument ioda and full name
+        # ---------------------------------
+        ioda_name = observation
+        full_name = ioda_name_to_long_name(ioda_name, self.logger)
+
+        # Create dictionary used to override the eva config
+        # -------------------------------------------------
+        eva_override = {}
+        eva_override['cycle_dir'] = self.cycle_dir()
+        eva_override['obs_path_file_1'] = obs_path_file_1
+        eva_override['obs_path_file_2'] = obs_path_file_2
+        eva_override['instrument'] = ioda_name
+        eva_override['instrument_title'] = full_name
+        eva_override['simulated_variables'] = \
+            observation_dict_1['obs space']['simulated variables']
+        eva_override['map_projection'] = 'plcarr'
+        eva_override['domain'] = 'global'
+        eva_override['experiment_id_1'] = experiment_id_1
+        eva_override['experiment_id_2'] = experiment_id_2
+
+        eva_override['experiment_tag_1'] = experiment_tag_1
+        eva_override['experiment_tag_2'] = experiment_tag_2
+
+        # If filename contains icec_ change map projection to polar stereographic
+        # -----------------------------------------------------------------------
+        if 'icec_' in obs_file_1:
+            eva_override['map_projection'] = 'npstere'
+            eva_override['domain'] = 'north'
+
+            # if file name has 'south" or "sh" then change to south polar stereographic
+            # ---------------------------------------------------------------
+            if 'south' in obs_file_1 or 'sh' in obs_file_1:
+                eva_override['map_projection'] = 'spstere'
+                eva_override['domain'] = 'south'
+
+        # # Check if the "passivate" condition exists within the "obs filters" list
+        passivate_exists = any(
+            filter_item.get('action', {}).get('name') == 'passivate'
+            for filter_item in observation_dict_1.get('obs filters', [])
+        )
+
+        if passivate_exists:
+            self.logger.info("Condition 'passivate' exists in 'obs filters'")
+            eva_override['passivated_variables'] = True
+
+        if 'channels' in observation_dict_1['obs space']:
+            need_channels = True
+            if observation in channels_to_plot:
+                eva_override['channels'] = channels_to_plot[observation]
             else:
-                obs_long_name = observation
+                eva_override['channels'] = observation_dict_1['obs space']['channels']
+        else:
+            need_channels = False
+            eva_override['channels'] = ''
+            eva_override['channel'] = ''
 
-            observation_dict_1 = None
-            for value in obs_config_1:
-                if value['obs space']['name'] == obs_long_name:
-                    observation_dict_1 = value.copy()
+        # Override the eva dictionary
+        # ---------------------------
+        eva_str = template_string_jinja2(self.logger, eva_str_template, eva_override)
+        eva_dict = yaml.safe_load(eva_str)
 
-            observation_dict_2 = None
-            for value in obs_config_2:
-                if value['obs space']['name'] == obs_long_name:
-                    observation_dict_2 = value.copy()
+        # Remove channel keys if not needed
+        # ---------------------------------
+        if not need_channels:
+            remove_matching_keys(eva_dict, 'channel')
+            remove_matching_keys(eva_dict, 'channels')
+            eva_dict = replace_string_in_dictionary(eva_dict, '${channel}', '')
 
-            if observation_dict_1 is None or observation_dict_2 is None:
-                continue
+        # Write eva dictionary to file
+        # ----------------------------
+        conf_output = os.path.join(self.cycle_dir(), 'eva', ioda_name, ioda_name+'_eva.yaml')
+        os.makedirs(os.path.dirname(conf_output), exist_ok=True)
+        with open(conf_output, 'w') as outfile:
+            yaml.dump(eva_dict, outfile, default_flow_style=False)
 
-            # Check if IODA observation input and output have non-zero location dimensions
-            use_obs_1 = check_obs(self.jedi_rendering.observing_system_records_path, observation,
-                                  observation_dict_1, self.cycle_time_dto(), input_and_output=True)
-
-            use_obs_2 = check_obs(self.jedi_rendering.observing_system_records_path, observation,
-                                  observation_dict_2, self.cycle_time_dto(), input_and_output=True)
-
-            use_obs = use_obs_1 and use_obs_2
-
-            if not use_obs:
-                continue
-
-            # Split the full path into path and filename
-            obs_path_file_1 = observation_dict_1['obs space']['obsdataout']['engine']['obsfile']
-            cycle_dir_1, obs_file_1 = os.path.split(obs_path_file_1)
-
-            # Split the full path into path and filename
-            obs_path_file_2 = observation_dict_2['obs space']['obsdataout']['engine']['obsfile']
-            cycle_dir_2, obs_file_2 = os.path.split(obs_path_file_2)
-
-            # Check for need to add 0000 to the file
-            # --------------------------------------
-            if not os.path.exists(obs_path_file_1):
-                obs_path_file_name, obs_path_file_ext = os.path.splitext(obs_path_file_1)
-                obs_path_file_0000 = obs_path_file_name + '_0000' + obs_path_file_ext
-                if not os.path.exists(obs_path_file_0000):
-                    self.logger.abort(f'No observation file found for {obs_path_file_1} or ' +
-                                      f'{obs_path_file_0000}')
-                obs_path_file_1 = obs_path_file_0000
-
-            # Check for need to add 0000 to the file
-            # --------------------------------------
-            if not os.path.exists(obs_path_file_2):
-                obs_path_file_name, obs_path_file_ext = os.path.splitext(obs_path_file_2)
-                obs_path_file_0000 = obs_path_file_name + '_0000' + obs_path_file_ext
-                if not os.path.exists(obs_path_file_0000):
-                    self.logger.abort(f'No observation file found for {obs_path_file_2} or ' +
-                                      f'{obs_path_file_0000}')
-                obs_path_file_2 = obs_path_file_0000
-
-            # Get instrument ioda and full name
-            # ---------------------------------
-            ioda_name = observation
-            full_name = ioda_name_to_long_name(ioda_name, self.logger)
-
-            # Create dictionary used to override the eva config
-            # -------------------------------------------------
-            eva_override = {}
-            eva_override['cycle_dir'] = self.cycle_dir()
-            eva_override['obs_path_file_1'] = obs_path_file_1
-            eva_override['obs_path_file_2'] = obs_path_file_2
-            eva_override['instrument'] = ioda_name
-            eva_override['instrument_title'] = full_name
-            eva_override['simulated_variables'] = \
-                observation_dict_1['obs space']['simulated variables']
-            eva_override['map_projection'] = 'plcarr'
-            eva_override['domain'] = 'global'
-            eva_override['experiment_id_1'] = experiment_id_1
-            eva_override['experiment_id_2'] = experiment_id_2
-
-            eva_override['experiment_tag_1'] = experiment_tag_1
-            eva_override['experiment_tag_2'] = experiment_tag_2
-
-            # If filename contains icec_ change map projection to polar stereographic
-            # -----------------------------------------------------------------------
-            if 'icec_' in obs_file_1:
-                eva_override['map_projection'] = 'npstere'
-                eva_override['domain'] = 'north'
-
-                # if file name has 'south" or "sh" then change to south polar stereographic
-                # ---------------------------------------------------------------
-                if 'south' in obs_file_1 or 'sh' in obs_file_1:
-                    eva_override['map_projection'] = 'spstere'
-                    eva_override['domain'] = 'south'
-
-            # # Check if the "passivate" condition exists within the "obs filters" list
-            passivate_exists = any(
-                filter_item.get('action', {}).get('name') == 'passivate'
-                for filter_item in observation_dict_1.get('obs filters', [])
-            )
-
-            if passivate_exists:
-                self.logger.info("Condition 'passivate' exists in 'obs filters'")
-                eva_override['passivated_variables'] = True
-
-            if 'channels' in observation_dict_1['obs space']:
-                need_channels = True
-                if observation in channels_to_plot:
-                    eva_override['channels'] = channels_to_plot[observation]
-                else:
-                    eva_override['channels'] = observation_dict_1['obs space']['channels']
-            else:
-                need_channels = False
-                eva_override['channels'] = ''
-                eva_override['channel'] = ''
-
-            # Override the eva dictionary
-            # ---------------------------
-            eva_str = template_string_jinja2(self.logger, eva_str_template, eva_override)
-            eva_dict = yaml.safe_load(eva_str)
-
-            # Remove channel keys if not needed
-            # ---------------------------------
-            if not need_channels:
-                remove_matching_keys(eva_dict, 'channel')
-                remove_matching_keys(eva_dict, 'channels')
-                eva_dict = replace_string_in_dictionary(eva_dict, '${channel}', '')
-
-            # Write eva dictionary to file
-            # ----------------------------
-            conf_output = os.path.join(self.cycle_dir(), 'eva', ioda_name, ioda_name+'_eva.yaml')
-            os.makedirs(os.path.dirname(conf_output), exist_ok=True)
-            with open(conf_output, 'w') as outfile:
-                yaml.dump(eva_dict, outfile, default_flow_style=False)
-
-            # Add eva dictionary to list
-            # --------------------------
-            eva_dicts.append(eva_dict)
-
-        # Call eva in parallel
-        # --------------------
-        with Pool(processes=number_of_workers) as pool:
-            pool.map(run_eva, eva_dicts)
+        # Call Eva
+        # --------
+        run_eva(eva_dict)
