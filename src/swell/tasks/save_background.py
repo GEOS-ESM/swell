8f8afc75b49a2a40f3f2b2dc4b677b255076ab7d
Furkan Goktas <ftgoktas@gmail.com>
2026-06-04 16:45:21 -0500

    Get model name with a function

--- src/swell/tasks/save_background.py
+++ src/swell/tasks/save_background.py
@@ -60,7 +60,7 @@ class SaveBackground(taskBase):
         # Cycle time is the forecast initialisation time
         forecast_start = dt.strptime(self.cycle_time(), datetime_formats['iso_format'])
 
-        model = self.__model__
+        model = self.get_model()
         source_template = self.config.background_source_path()
         experiment = self.config.background_experiment('geos_cf_v2')
         resolution = self.config.horizontal_resolution('c360')
