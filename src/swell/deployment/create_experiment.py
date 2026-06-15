a1ad38b776130997abf542587424c17757d67262
Michael Anstett <michael.anstett@nasa.gov>
2026-06-15 17:19:07 -0400

    Give LocalEnsembleDa more time

--- src/swell/deployment/create_experiment.py
+++ src/swell/deployment/create_experiment.py
@@ -576,6 +576,7 @@ def prepare_cylc_suite_jinja2(
     render_dictionary['scheduling']['EvaObservations']['execution_time_limit'] = 'PT30M'
     render_dictionary['scheduling']['GenerateBClimatology']['execution_time_limit'] = 'PT10M'
     render_dictionary['scheduling']['RunGeos']['execution_time_limit'] = 'PT30M'
+    render_dictionary['scheduling']['RunJediLocalEnsembleDaExecutable'] = 'PT40M'
 
     # Set jinja templated string to use upon runtime
     # ----------------------------------------------
