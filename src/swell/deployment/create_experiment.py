e29104e409f1ee11d9b3064226f256ad548b5eb0
Michael Anstett <michael.anstett@nasa.gov>
2026-06-16 09:33:14 -0400

    Give 1 hour

--- src/swell/deployment/create_experiment.py
+++ src/swell/deployment/create_experiment.py
@@ -577,7 +577,7 @@ def prepare_cylc_suite_jinja2(
     render_dictionary['scheduling']['GenerateBClimatology']['execution_time_limit'] = 'PT10M'
     render_dictionary['scheduling']['RunGeos']['execution_time_limit'] = 'PT30M'
     render_dictionary['scheduling']['RunJediLocalEnsembleDaExecutable'][
-            'execution_time_limit'] = 'PT40M'
+            'execution_time_limit'] = 'PT1H'
 
     # Set jinja templated string to use upon runtime
     # ----------------------------------------------
