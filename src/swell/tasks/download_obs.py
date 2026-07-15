8df3a0e3c4f18d5430c3cc917feaa0eee7d284c9
jeromebarre <jerome.e.barre@gmail.com>
2026-07-15 09:21:24 -0400

    download only relevant hours with s3 method

--- src/swell/tasks/download_obs.py
+++ src/swell/tasks/download_obs.py
@@ -264,6 +264,7 @@ class DownloadObs(taskBase):
                 'Install it with: pip install boto3')
 
         s3_source_template = obs_config['s3_source']
+        filename_pattern = obs_config.get('filename_pattern', '')
         max_orbit_dur = isodate.parse_duration(
             obs_config.get('max_orbit_duration', 'PT0H'))
 
@@ -280,8 +281,10 @@ class DownloadObs(taskBase):
         if dry_run:
             for day_date in self._day_slots(search_start, search_end):
                 prefix = self._resolve_path(prefix_template, day_date)
+                pattern_info = (f' matching {self._resolve_path(filename_pattern, day_date)}'
+                                if filename_pattern else '')
                 self.logger.info(
-                    f'  [DRY RUN] Would list s3://{bucket}/{prefix}')
+                    f'  [DRY RUN] Would list s3://{bucket}/{prefix}{pattern_info}')
             return 0, 0
 
         s3_client = boto3.client(
@@ -313,6 +316,20 @@ class DownloadObs(taskBase):
                 self.logger.info(
                     f'  No objects found under s3://{bucket}/{prefix}')
                 continue
+
+            # download only relevant windows
+            if filename_pattern:
+                valid_hours = [h for d, h in self._hour_slots(search_start, search_end)
+                               if d == day_date]
+                hours_window = [
+                    re.compile(
+                        '^' + re.escape(
+                            self._resolve_filename(filename_pattern, day_date, h)
+                        ).replace(r'\*', '.*') + '$')
+                    for h in valid_hours
+                ]
+                keys = [k for k in keys
+                        if any(r.match(os.path.basename(k)) for r in hours_window)]
 
             self.logger.info(
                 f'  {day_date}: {len(keys)} object(s) found')
