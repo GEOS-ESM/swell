93d9f4a53f83bed7d5488bdb545a202ddb14d953
Yonggang Yu <yonggang.yu@nasa.gov>
2026-07-22 14:24:13 -0600

    set slurm directives

--- src/swell/deployment/create_experiment.py
+++ src/swell/deployment/create_experiment.py
@@ -589,6 +589,10 @@ def prepare_cylc_suite_jinja2(
     render_dictionary['scheduling']['RunGeos']['execution_time_limit'] = 'PT30M'
     render_dictionary['scheduling']['RunJediLocalEnsembleDaExecutable'][
             'execution_time_limit'] = 'PT1H'
+    render_dictionary['scheduling']['RunJediEdaExecutable'][
+            'execution_time_limit'] = 'PT30M'
+    render_dictionary['scheduling']['RunJediEdaControlPertExecutable'][
+            'execution_time_limit'] = 'PT30M'
 
     # Set jinja templated string to use upon runtime
     # ----------------------------------------------
