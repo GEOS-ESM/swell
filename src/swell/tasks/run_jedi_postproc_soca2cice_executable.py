d95c48571ecdf99b1a349e2a8ced3d2733a81190
dooruk <dorukhan.ardag@nasa.gov>
2026-08-04 15:43:37 -0400

    new postproc task using different executable

---
+++ src/swell/tasks/run_jedi_postproc_soca2cice_executable.py
@@ -0,0 +1,114 @@
+# (C) Copyright 2021- United States Government as represented by the Administrator of the
+# National Aeronautics and Space Administration. All Rights Reserved.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+
+
+# --------------------------------------------------------------------------------------------------
+
+
+import os
+from ruamel.yaml import YAML
+
+from swell.tasks.base.task_base import taskBase
+from swell.utilities.run_jedi_executables import run_executable
+
+
+# --------------------------------------------------------------------------------------------------
+
+
+class RunJediPostprocSoca2ciceExecutable(taskBase):
+
+    # ----------------------------------------------------------------------------------------------
+
+    def execute(self) -> None:
+
+        # Jedi application name
+        # ---------------------
+        jedi_application = 'postproc_soca2cice'
+
+        generate_yaml_and_exit = self.config.generate_yaml_and_exit(False)
+        window_type = self.config.window_type()
+        window_length = self.config.window_length()
+
+        # Compute data assimilation window parameters
+        # --------------------------------------------
+        analysis_time = self.da_window_params.analysis_time()
+        analysis_time_iso = self.da_window_params.analysis_time_iso()
+        local_background_time = self.da_window_params.local_background_time(window_length,
+                                                                            window_type)
+        local_background_time_iso = self.da_window_params.local_background_time_iso(window_length,
+                                                                                    window_type)
+
+        # Populate jedi interface templates dictionary
+        # --------------------------------------------
+        self.jedi_rendering.add_key('analysis_variables', self.config.analysis_variables())
+        self.jedi_rendering.add_key('marine_models', self.config.marine_models(None))
+
+        # Background and analysis times
+        # -----------------------------
+        self.jedi_rendering.add_key('local_background_time', local_background_time)
+        self.jedi_rendering.add_key('local_background_time_iso', local_background_time_iso)
+        self.jedi_rendering.add_key('analysis_time', analysis_time)
+        self.jedi_rendering.add_key('analysis_time_iso', analysis_time_iso)
+
+        # Add placeholder names if mock experiment
+        # ----------------------------------------
+        if self.config.mock_experiment(False):
+            self.jedi_rendering.add_key('experiment_root', 'experiment_root')
+            self.jedi_rendering.add_key('experiment_id', 'experiment_id')
+            self.jedi_rendering.add_key('cycle_dir', 'cycle_dir')
+
+        # Geometry
+        # --------
+        self.jedi_rendering.add_key('total_processors', self.config.total_processors(None))
+
+        # Jedi configuration file
+        # -----------------------
+        jedi_config_file = os.path.join(self.cycle_dir(),
+                                        f'jedi_{jedi_application}_config.yaml')
+
+        # Output log file
+        # ---------------
+        output_log_file = os.path.join(self.cycle_dir(),
+                                       f'jedi_{jedi_application}_log.log')
+
+        # Open the JEDI config file and fill templates
+        # --------------------------------------------
+        jedi_config_dict = self.jedi_rendering.render_oops_file(f'{jedi_application}',
+                                                                window_type,
+                                                                jedi_forecast_model)
+
+        yaml = YAML()
+        yaml.default_flow_style = False
+
+        # Write the expanded dictionary to YAML file
+        # ------------------------------------------
+        with open(jedi_config_file, 'w') as jedi_config_file_open:
+            yaml.dump(jedi_config_dict, jedi_config_file_open)
+
+        # Get the JEDI interface metadata
+        # -------------------------------
+        model_component_meta = self.jedi_rendering.render_interface_meta()
+
+        # Compute number of processors
+        # ----------------------------
+        np = eval(str(model_component_meta['total_processors']))
+
+        # Jedi executable name
+        # --------------------
+        jedi_executable = model_component_meta['executables'][f'{jedi_application}']
+        jedi_executable_path = os.path.join(self.experiment_path(), 'jedi_bundle',
+                                            'build', 'bin', jedi_executable)
+
+        # Run the JEDI executable
+        # -----------------------
+        if not generate_yaml_and_exit:
+            self.logger.info('Running '+jedi_executable_path+' with '+str(np)+' processors.')
+            run_executable(self.logger, self.cycle_dir(), np, jedi_executable_path,
+                           jedi_config_file, output_log_file)
+        else:
+            self.logger.info('YAML generated, now exiting.')
+
+# --------------------------------------------------------------------------------------------------
