c7afeb0ae9d71d426cdbf55cfceef47e2b93e807
dooruk <dorukhan.ardag@nasa.gov>
2026-08-26 12:41:15 -0400

    just to trigger ci

--- src/swell/tasks/prep_coupled_geos_run_dir.py
+++ src/swell/tasks/prep_coupled_geos_run_dir.py
@@ -24,7 +24,7 @@ class PrepCoupledGeosRunDir(taskBase):
     def execute(self) -> None:
 
         """
-        Copies GEOS HOMDIR files to the cycle forecast directory to prepare before executing
+        Copies GEOS HOMDIR files to the GEOSfcm forecast directory and prepares it before executing
         gcm_run.j.
         Modifies certain resource files using python's re package according to cycle_date such as:
         CAP.rc, AGCM.rc, input.nml, and gcm_run.j.
